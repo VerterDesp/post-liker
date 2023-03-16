718d3c55ae56940051685777758a238ddbad93ec
VerterDesp <allaknar@gmail.com>
2023-03-17 00:49:40 +0200

    first commit

---
+++ src/main/java/com/postliker/PostLikerApplication.java
@@ -0,0 +1,13 @@
+package com.postliker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostLikerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PostLikerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/postliker/PostLikerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postliker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostLikerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
