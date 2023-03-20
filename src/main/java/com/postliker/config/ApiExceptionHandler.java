194af3e96fd0c6114a9ed8d336b324cf89273bfa
VerterDesp <allaknar@gmail.com>
2023-03-20 04:18:03 +0200

    refactor showing custom exceptions message

--- src/main/java/com/postliker/config/ApiExceptionHandler.java
+++ src/main/java/com/postliker/config/ApiExceptionHandler.java
@@ -2,46 +2,41 @@ package com.postliker.config;
 
 import com.postliker.exception.*;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApiExceptionHandler {
 
   @ExceptionHandler(EmailAlreadyRegisteredException.class)
-  public ResponseEntity<String> handleEmailAlreadyRegisteredException( EmailAlreadyRegisteredException exception ) {
-    return ResponseEntity
-      .status(HttpStatus.BAD_REQUEST)
-      .body(exception.getMessage());
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public String handleEmailAlreadyRegisteredException( EmailAlreadyRegisteredException exception ) {
+    return exception.getMessage();
   }
 
   @ExceptionHandler(EditAlienPostException.class)
-  public ResponseEntity<String> handleEditAlienPostException( EditAlienPostException exception ) {
-    return ResponseEntity
-      .status(HttpStatus.FORBIDDEN)
-      .body(exception.getMessage());
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  public String handleEditAlienPostException( EditAlienPostException exception ) {
+    return exception.getMessage();
   }
 
   @ExceptionHandler(DeleteAlienPostException.class)
-  public ResponseEntity<String> handleDeleteAlienPostException( DeleteAlienPostException exception ) {
-    return ResponseEntity
-      .status(HttpStatus.FORBIDDEN)
-      .body(exception.getMessage());
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  public String handleDeleteAlienPostException( DeleteAlienPostException exception ) {
+    return exception.getMessage();
   }
 
   @ExceptionHandler(PostDoesntExistException.class)
-  public ResponseEntity<String> handlePostDoesntExistException( PostDoesntExistException exception ) {
-    return ResponseEntity
-      .status(HttpStatus.NOT_FOUND)
-      .body(exception.getMessage());
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String handlePostDoesntExistException( PostDoesntExistException exception ) {
+    return exception.getMessage();
   }
 
   @ExceptionHandler(BadCredentialsException.class)
-  public ResponseEntity<String> handleBadCredentialsException() {
-    return ResponseEntity
-      .status(HttpStatus.FORBIDDEN)
-      .body("No user with given credentials!");
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  public String handleBadCredentialsException() {
+    return "No user with given credentials!";
   }
 }
